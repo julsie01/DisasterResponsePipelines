fa9fc845bad526a7d31310378fe6b48bbea3875e
julsie01 <38508211+julsie01@users.noreply.github.com>
2018-12-27 18:53:18 +0000

    add setting to make grid search optional

--- model/train_classifier.py
+++ model/train_classifier.py
@@ -231,11 +231,12 @@ def save_model(model, model_filepath):
 
 
 def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
+    if len(sys.argv) == 4:
+        database_filepath, model_filepath, run_model_tuning = sys.argv[1:]
+        print(run_model_tuning)
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
         
         print('Building model...')
         model = build_model()
@@ -251,10 +252,10 @@ def main():
 
         print('Trained model saved!')
 
-        print('Tuning Model')
-        best_model = tune_model(model, X_train, Y_train, True)
-
-        save_model(best_model, 'best_estimator.pkl')
+        if run_model_tuning == '1':
+            print('Tuning Model')
+            best_model = tune_model(model, X_train, Y_train, True)
+            save_model(best_model, model_filepath)
 
     else:
         print('Please provide the filepath of the disaster messages database '\
