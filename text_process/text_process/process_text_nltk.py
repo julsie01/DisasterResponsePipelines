5cd5c619271279f3aa47e5294b765a711e8e9d95
julsie01 <38508211+julsie01@users.noreply.github.com>
2018-12-28 10:14:36 +0000

    update doc strings for text processing package

--- text_process/text_process/process_text_nltk.py
+++ text_process/text_process/process_text_nltk.py
@@ -6,22 +6,40 @@ from nltk.stem.wordnet import WordNetLemmatizer
 
 class TextFeatureExtractor:
 
+
     def strip_stop_words(self, words):
-    
+        """
+        Funcion to remove stop words from a list of words
+        Input: List of Words
+        Output: List of Words excluding nltk stopwords
+        """
         words = [w for w in words if w not in stopwords.words("english")]
         return words
 
     def stem(self, words):
-    
+        """
+        Funcion to reduce words to their base word (stemming)
+        Input: List of Words
+        Output: List of Words Stemmed using NLTK porter stemmer
+        """
         stemmed = [PorterStemmer().stem(w) for w in words]
         return stemmed
 
     def lemmatize(self, words):
-
+        """
+        Funcion to lematize a list of words
+        Input: List of Words
+        Output: List of Words Lemmatized using NLTK Word Net Lemmatizer
+        """
         lemmed = [WordNetLemmatizer().lemmatize(w) for w in words]
         return lemmed
 
     def tokenize(self, text):
+        """
+        Funcion to convert a text string to a list of individual words
+        Input: text: String of Text
+        Output: List of individual Words
+        """
         words = word_tokenize(text)
         return words
 
